e39a604ba3ac3e6ddd6fbb8499adef5a0a36551a
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-10-27 11:27:39 +0600

    Add error page and do routing

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div className="h-screen w-full flex justify-center items-center">
+            <h1 className="text-6xl">404: Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
