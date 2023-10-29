f24ca8c99594fb8362099a2c32cc36944dddcd37
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-10-29 13:08:17 +0600

    Add password validation and error handling for Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const Register = () => {
     const { createUser, updateUser } = useContext(AuthContext);
+    const [error, setError] = useState();
 
     const handleRegister = (e) => {
         e.preventDefault();
@@ -14,20 +16,45 @@ const Register = () => {
         const email = form.get("email");
         const password = form.get("password");
         console.log(name, photoURL, email, password);
-        createUser(email, password)
-            .then((result) => {
-                console.log(result.user);
-                updateUser(result.user, name, photoURL)
-                    .then(() => {
-                        console.log("User profile updated successfully");
-                    })
-                    .catch((error) => {
-                        console.error("Error updating user profile:", error);
-                    });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        // reset error
+        setError("");
+        if (password.length < 6) {
+            setError("Error: Password should be 6+ characters long !");
+        } else if (/^[^A-Z]*$/.test(password)) {
+            setError(
+                "Error: Password should have at lease one capital letter !"
+            );
+        } else if (/^[a-zA-Z0-9]*$/.test(password)) {
+            setError(
+                "Error: Password should have at lease one special character !"
+            );
+        }
+        if (error === "") {
+            createUser(email, password)
+                .then((result) => {
+                    console.log(result.user);
+                    Swal.fire(
+                        "Successful !",
+                        "User successfully registered !",
+                        "success"
+                    );
+                    updateUser(result.user, name, photoURL)
+                        .then(() => {
+                            console.log("User profile updated successfully");
+                        })
+                        .catch((error) => {
+                            console.error(
+                                "Error updating user profile:",
+                                error
+                            );
+                            setError(error.message);
+                        });
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setError(error.message);
+                });
+        }
     };
 
     return (
@@ -86,6 +113,7 @@ const Register = () => {
                             className="input input-bordered"
                         />
                     </div>
+                    {error && <p className="text-red-700">{error}</p>}
                     <div className="form-control mt-6">
                         <button className="btn btn-primary">Register</button>
                     </div>
