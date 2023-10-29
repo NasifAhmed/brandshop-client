abfd4c06146d2886c89d22c481818a189758fbc6
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-10-29 13:07:39 +0600

    Make slider auto slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,6 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Link } from "react-router-dom";
 
 // Import Swiper styles
 import "swiper/css";
@@ -22,28 +23,34 @@ const Slider = () => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div className="w-full flex justify-center items-center">
-                        <img
-                            src="https://toyotapromodealer.com/wp-content/uploads/2022/10/home-banner-camry-1293x820-1.jpg"
-                            className="max-h-96"
-                        />
-                    </div>
+                    <Link to={`/ProductDetails/Camry`}>
+                        <div className="w-full flex justify-center items-center">
+                            <img
+                                src="https://toyotapromodealer.com/wp-content/uploads/2022/10/home-banner-camry-1293x820-1.jpg"
+                                className="max-h-96"
+                            />
+                        </div>
+                    </Link>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="w-full flex justify-center items-center">
-                        <img
-                            src="https://www.honda.com.sg/images/cars/2021_All-New_Civic/Launch/All-New-Civic_Model-Page-Banner-v1---1920x888px.jpg"
-                            className="max-h-96"
-                        />
-                    </div>
+                    <Link to={`/ProductDetails/Civic`}>
+                        <div className="w-full flex justify-center items-center">
+                            <img
+                                src="https://www.honda.com.sg/images/cars/2021_All-New_Civic/Launch/All-New-Civic_Model-Page-Banner-v1---1920x888px.jpg"
+                                className="max-h-96"
+                            />
+                        </div>
+                    </Link>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="w-full flex justify-center items-center">
-                        <img
-                            src="https://oomdo.com/landingPageTool/Content/HCJD/2022/Nissan/Altima/ADHN_22ALTIMA_HERO_LP_1_2022.jpg"
-                            className="max-h-96"
-                        />
-                    </div>
+                    <Link to={`/ProductDetails/Camry`}>
+                        <div className="w-full flex justify-center items-center">
+                            <img
+                                src="https://oomdo.com/landingPageTool/Content/HCJD/2022/Nissan/Altima/ADHN_22ALTIMA_HERO_LP_1_2022.jpg"
+                                className="max-h-96"
+                            />
+                        </div>
+                    </Link>
                 </SwiperSlide>
             </Swiper>
         </>
